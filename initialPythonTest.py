447440c45e4336ee451dc78a131620f97d28b791
Alex Dulemba <olekdulemba@gmail.com>
2020-07-21 19:58:33 -0400

    Modified first test file

--- initialPythonTest.py
+++ initialPythonTest.py
@@ -1,1 +1,13 @@
-print("Initial Git Test")
+# in order to use matplotlib normally, need to launch XLaunch first before running code 
+import numpy as np
+import matplotlib.pyplot as plt
+
+print("Initial Git Test") 
+
+t = np.arange(0, 25, 0.1)
+f = np.sin(t)
+
+fig = plt.subplots()
+plt.plot(t, f)
+plt.show()
+ 
