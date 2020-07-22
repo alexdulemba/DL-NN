ec406a24e860a7cfb89d472887a53ad880f64731
Alex Dulemba <olekdulemba@gmail.com>
2020-07-21 20:00:46 -0400

    Corrected comments

--- initialPythonTest.py
+++ initialPythonTest.py
@@ -1,4 +1,6 @@
-# in order to use matplotlib normally, need to launch XLaunch first before running code 
+# runs well on WSL2 for windows
+# in order to use matplotlib normally, need to launch XLaunch first before running code in order to display plot
+
 import numpy as np
 import matplotlib.pyplot as plt
 
